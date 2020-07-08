1f814d24634ec1968ab94789e56ed1440e05847f
Alexander Zhao <azhao6763@gmail.com>
2020-07-08 11:16:07 -0500

    'test'

--- PrimeNumberV2/PrimeNumberV2.cpp
+++ PrimeNumberV2/PrimeNumberV2.cpp
@@ -1,20 +1,50 @@
-// PrimeNumberV2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// PrimeNumberGenerator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Test Commit
 
 #include <iostream>
+#include <fstream>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int low, high, i, flag;
+
+    cout << "Enter two numbers(intervals): ";
+    cin >> low >> high;
+
+    ofstream myfile("C:\\MyWorkspace\\Temp\\Prime_Number_Generator.txt");
+
+    if (myfile.is_open())
+    {
+        myfile << "Prime numbers between " << low << " and " << high << " are: \n";
+        while (low < high)
+        {
+            flag = 0;
+
+            for (i = 2; i <= low / 2; ++i)
+            {
+                if (low % i == 0)
+                {
+                    flag = 1;
+                    break;
+                }
+            }
+
+            if (flag == 0)
+                myfile << low << " ";
+
+            ++low;
+        }
+
+        myfile << "\nPress x to exit the file. \n";
+        myfile.close();
+    }
+    else cout << "Unable to open file";
+
+
+
+
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
